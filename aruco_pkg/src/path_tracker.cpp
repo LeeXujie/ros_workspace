32cc4c6fcd3427d9e9b2cbe23734af71d23ac03a
LeeXujie <1808511884@qq.com>
2018-12-04 14:25:30 +0800

    update path_tracker.cpp

--- aruco_pkg/src/path_tracker.cpp
+++ aruco_pkg/src/path_tracker.cpp
@@ -6,39 +6,143 @@
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-
-float calculateDistance(Point3f pt1, Point3f pt2) {
-  return sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) +
-              (pt1.y - pt2.y) * (pt1.y - pt2.y) +
-              (pt1.z - pt2.z) * (pt1.z - pt2.z));
-}
+#include <Eigen/Geometry>
+#include <opencv2/core/eigen.hpp>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <aruco_pkg/robot_stop.h>
 
 class pathTracker {
 private:
   ros::NodeHandle nh;
   nav_msgs::Path path;
+  ros::ServiceServer srv;
   ros::Subscriber sub;
   ros::Publisher path_pub;
-  ros::Publisher marker_pub;
+  ros::Publisher teachPoint_pub;
+  ros::Publisher targetPoint_pub;
+  ros::Publisher twist_pub;
+  ros::Publisher robotPose_pub;
   std::string path_file;
+  geometry_msgs::Twist moto_control;
   geometry_msgs::PoseStamped this_pose_stamped;
 
+  Mutex m_lock;
   Mat teachPath;
-  int rowcount, colcount;
+  float angle;
+  bool forward_flag,stop_flag;
+  int n, foresee, targetPosition;
+  int rowcount, colcount, speedcount;
   int initialCount, currentPositon, lastPosition;
 
+  float calculateDistance(Point3f pt1, Point3f pt2) {
+    return sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) +
+                (pt1.y - pt2.y) * (pt1.y - pt2.y) +
+                (pt1.z - pt2.z) * (pt1.z - pt2.z));
+  }
+
+  float getAngelOfTwoVector(Point2f &pt1, Point2f &pt2, Point2f &c) {
+    float theta = atan2(pt1.x - c.x, pt1.y - c.y) - atan2(pt2.x - c.x, pt2.y - c.y);
+    if (theta > CV_PI) theta -= 2 * CV_PI;
+    if (theta < -CV_PI) theta += 2 * CV_PI;
+    theta = theta * 180.0 / CV_PI;
+    return theta;
+  }
+
 public:
-  pathTracker() : nh("~") {
+  pathTracker() : nh("~"),foresee(25) {
     nh.param<std::string>("path_file", path_file, "navigation_path.txt");
 
     path.header.frame_id = "odom";
     path.header.stamp = ros::Time(0);
-    path_pub = nh.advertise<nav_msgs::Path>("teaching_path", 10, true);
-    marker_pub = nh.advertise<visualization_msgs::Marker>("teaching_point", 10);
+    path_pub = nh.advertise<nav_msgs::Path>("teaching_path", 1, true);
+    teachPoint_pub = nh.advertise<visualization_msgs::Marker>("teaching_point", 1);
+    targetPoint_pub = nh.advertise<visualization_msgs::Marker>("target_point", 1);
+    twist_pub = nh.advertise<geometry_msgs::Twist>("/turtlebot_teleop/cmd_vel",1);
+    robotPose_pub = nh.advertise<geometry_msgs::PoseStamped>("robotpose",1);
     sub = nh.subscribe("/vo_data", 10, &pathTracker::pose_track, this);
+    srv = nh.advertiseService("stop_robot",&pathTracker::stop,this);
 
     readFile_();
     initialCount = 0;
+    speedcount = 0;
+    targetPosition = rowcount-500;
+    forward_flag=true;
+    stop_flag=false;
+  }
+  ~pathTracker()
+  {
+  }
+
+  bool stop(aruco_pkg::robot_stop::Request &req, aruco_pkg::robot_stop::Response &res)
+  {
+    m_lock.lock();
+    if(req.stop_flag){
+      stop_flag=true;
+      geometry_msgs::Twist robotStop;
+      robotStop.linear.x=0;
+      robotStop.angular.z=0;
+      twist_pub.publish(robotStop);
+      res.robot_status="robot is stopped!\n";}
+    if(!req.stop_flag){stop_flag=false;res.robot_status="robot is running!\n";}
+    m_lock.unlock();
+  }
+
+  void robotPosePub(cv::Point3f robotPosition_,Eigen::Quaterniond robotOrientation_)
+  {
+    geometry_msgs::PoseStamped robotPose;
+    robotPose.header.frame_id="odom";
+    robotPose.header.stamp=ros::Time::now();
+    robotPose.pose.position.x=robotPosition_.x;
+    robotPose.pose.position.y=robotPosition_.y;
+    robotPose.pose.position.z=robotPosition_.z;
+    robotPose.pose.orientation.w=robotOrientation_.w();
+    robotPose.pose.orientation.x=robotOrientation_.x();
+    robotPose.pose.orientation.y=robotOrientation_.y();
+    robotPose.pose.orientation.z=robotOrientation_.z();
+    robotPose_pub.publish(robotPose);
+  }
+
+  void navigation(cv::Point3f camPosition_,Eigen::Quaterniond camPose_,cv::Point3f teachPoint_)
+  {
+    Eigen::AngleAxisd t_V(M_PI / 2, Eigen::Vector3d(0.0, 1.0, 0.0));
+    Eigen::Matrix3d robotPose = camPose_.matrix()*t_V.matrix();
+
+    Mat robotPoseMat;
+    cv::eigen2cv(robotPose,robotPoseMat);
+    Mat vect = (Mat_<double>(3, 1) << 0.0, 0.0, 1.0);
+    Mat robotVect = robotPoseMat * vect;
+    robotVect.convertTo(robotVect,CV_32F);
+
+    Point2f c(0.0, 0.0);
+    Point2f pt1(teachPoint_.x-camPosition_.x, teachPoint_.z-camPosition_.z);
+    Point2f pt2(robotVect.at<float>(0,0), robotVect.at<float>(2,0));
+    if(!forward_flag){pt2.x=-robotVect.at<float>(0,0);pt2.y=-robotVect.at<float>(2,0);}
+    angle=getAngelOfTwoVector(pt1,pt2,c);
+    
+    if(speedcount<100)speedcount++;
+    if(forward_flag){
+      robotPosePub(camPosition_,camPose_);
+      if(abs(n)<foresee)moto_control.linear.x=0.006*n+0.05;
+      else moto_control.linear.x=0.01*speedcount*0.2;
+    }
+    else{
+      Eigen::AngleAxisd V(M_PI, Eigen::Vector3d(0.0, 1.0, 0.0));
+      Eigen::Quaterniond VQ(V);
+      robotPosePub(camPosition_,camPose_*VQ);
+      if(abs(n)<foresee)moto_control.linear.x=0.006*n-0.05;
+      else moto_control.linear.x=-0.01*speedcount*0.2;
+    }
+    moto_control.angular.z=angle/(360)*abs(n)*0.04;
+    if(moto_control.angular.z>0.3)moto_control.angular.z=0.3;
+    if(moto_control.angular.z<-0.3)moto_control.angular.z=-0.3;
+
+    m_lock.lock();
+    if(!stop_flag){
+      ROS_INFO("linear: %.3lf, angular: %.3lf", moto_control.linear.x, moto_control.angular.z);
+      twist_pub.publish(moto_control);
+    }
+    m_lock.unlock();
   }
 
   void pose_track(const nav_msgs::OdometryConstPtr &odometry_msg) {
@@ -47,6 +151,8 @@ public:
     odomPoint.x = odometry_msg->pose.pose.position.x;
     odomPoint.y = odometry_msg->pose.pose.position.y;
     odomPoint.z = odometry_msg->pose.pose.position.z;
+    Eigen::Quaterniond odomPose(odometry_msg->pose.pose.orientation.w,odometry_msg->pose.pose.orientation.x,
+                         odometry_msg->pose.pose.orientation.y,odometry_msg->pose.pose.orientation.z);//w,x,y,z
 
 /**********************在示教路径中找到一个与当前位置最近的点**********************/
     if (initialCount < 10) //前十次的滤波值舍弃，让其逼近真实值
@@ -107,11 +213,33 @@ public:
       }
 /*******************到这里结束，找到点的位置为currentPosition*******************/
 /********下面就要将currentPositon的三维坐标以marker点的形式显示到示教路径中********/
+      if(forward_flag){
+        targetPosition=rowcount-500;
+        if(targetPosition-currentPositon>foresee)n=foresee;
+        else n=targetPosition-currentPositon;
+        if(n<=0){forward_flag=!forward_flag;speedcount=0;}
+      }
+      else{
+        targetPosition=100;
+        if(currentPositon-targetPosition>foresee)n=-foresee;
+        else n=targetPosition-currentPositon;
+        if(n>=0){forward_flag=!forward_flag;speedcount=0;}
+      }
+
       geometry_msgs::Point point;
-      point.x = teachPath.at<float>(currentPositon, 1);
-      point.y = teachPath.at<float>(currentPositon, 2);
-      point.z = teachPath.at<float>(currentPositon, 3);
-      pub_marker(point, odometry_msg->header);
+      teachPoint.x=teachPath.at<float>(currentPositon+n,1);
+      teachPoint.y=teachPath.at<float>(currentPositon+n,2);
+      teachPoint.z=teachPath.at<float>(currentPositon+n,3);
+      point.x=teachPoint.x;point.y=teachPoint.y;point.z=teachPoint.z;
+      pub_marker(teachPoint_pub, point, odometry_msg->header, 0, 1.0, 1.0, 0.0);
+
+      cv::Point3f targetPoint(teachPath.at<float>(targetPosition,1),
+                              teachPath.at<float>(targetPosition,2),
+                              teachPath.at<float>(targetPosition,3));
+      point.x=targetPoint.x;point.y=targetPoint.y;point.z=targetPoint.z;
+      pub_marker(targetPoint_pub,point, odometry_msg->header, 1, 1.0, 0.0, 0.0);
+
+      navigation(odomPoint,odomPose,teachPoint);
     }
     lastPosition = currentPositon;
   }
@@ -140,26 +268,27 @@ public:
     }
   }
 
-  void pub_path() { path_pub.publish(path); }
-
-  void pub_marker(geometry_msgs::Point &point, const std_msgs::Header &header){
+  void pub_marker(ros::Publisher publisher, geometry_msgs::Point &point, const std_msgs::Header &header,
+                  int32_t marker_id, float r, float g, float b){
     visualization_msgs::Marker marker;
     marker.header = header;
     marker.ns = "point_shape";
-    marker.id = 0;
+    marker.id = marker_id;
     marker.type = visualization_msgs::Marker::SPHERE;
     marker.action = visualization_msgs::Marker::ADD;
     marker.pose.position = point;
     marker.scale.x = 0.1;
     marker.scale.y = 0.1;
     marker.scale.z = 0.1;
-    marker.color.r = 1.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
+    marker.color.r = r;
+    marker.color.g = g;
+    marker.color.b = b;
     marker.color.a = 1.0;
     marker.lifetime = ros::Duration();
-    marker_pub.publish(marker);
+    publisher.publish(marker);
   }
+
+  void pub_path() { path_pub.publish(path); }
 };
 
 int main(int argc, char **argv) {
